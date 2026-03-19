21979994424f692c403c735759271b3f7688437a
Louis <louis.lacoste@agroparistech.fr>
2026-03-19 16:28:57 +0100

    Adding Rcpp to handle errors

--- src/dispatcher.cc
+++ src/dispatcher.cc
@@ -6,6 +6,8 @@ SEXP dispatcher(SEXP sexp_membership_name,
                 SEXP sexp_network_from_R,
                 SEXP sexp_real_EM)
 {
+    BEGIN_RCPP
+
     std::string membership_name = Rcpp::as<std::string>(sexp_membership_name);
     Rcpp::List membership_init_from_R(sexp_membership_init_from_R);
     std::string model_name = Rcpp::as<std::string>(sexp_model_name);
@@ -22,6 +24,8 @@ SEXP dispatcher(SEXP sexp_membership_name,
                                                   membership_init_from_R,
                                                   model_name,
                                                   network_from_R);
+
+    END_RCPP
 }
     
 
