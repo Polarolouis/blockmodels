6150ce4d233789896dc517f7f750565fee92bb9a
Louis <louis.lacoste@agroparistech.fr>
2026-03-19 16:02:59 +0100

    test case for NAs

--- tests/BM_poisson.R
+++ tests/BM_poisson.R
@@ -68,3 +68,45 @@ M<-matrix(
 my_model <- BM_poisson("LBM",M , plotting='', explore_min=2, explore_max=2, ncores=2, verbosity=0)
 my_model$estimate()
 which.max(my_model$ICL)
+
+##
+## NA handling (SBM)
+##
+
+npc_na <- 10
+Q_na <- 2
+n_na <- npc_na * Q_na
+Z_na <- diag(Q_na) %x% matrix(1, npc_na, 1)
+L_na <- 70 * matrix(runif(Q_na * Q_na), Q_na, Q_na)
+M_expect_na <- Z_na %*% L_na %*% t(Z_na)
+M_na <- matrix(
+    rpois(
+        length(as.vector(M_expect_na)),
+        as.vector(M_expect_na)
+    ),
+    n_na,
+    n_na
+)
+
+off_diag <- which(row(M_na) != col(M_na))
+set.seed(123)
+M_na[sample(off_diag, max(1, floor(0.1 * length(off_diag))))] <- NA
+
+my_model_na <- BM_poisson("SBM", M_na, plotting='', explore_min=2, explore_max=2, ncores=2, verbosity=0)
+my_model_na$estimate()
+stopifnot(length(my_model_na$ICL) > 0)
+stopifnot(all(is.finite(my_model_na$ICL)))
+
+M_all_na <- matrix(NA_real_, nrow(M_na), ncol(M_na))
+has_error <- FALSE
+tryCatch(
+    {
+        my_model_all_na <- BM_poisson("SBM", M_all_na, plotting='', explore_min=2, explore_max=2, ncores=2, verbosity=0)
+        my_model_all_na$estimate()
+    },
+    error = function(e)
+    {
+        has_error <<- TRUE
+    }
+)
+stopifnot(has_error)
