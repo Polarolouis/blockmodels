e7389db8d8ff97847a34b3815ba741dfd0d157ca
Louis <louis.lacoste@agroparistech.fr>
2026-03-20 11:47:55 +0100

    Benchmarking for Beta recovery

--- dev/bench_beta_recov_sbm.R
+++ dev/bench_beta_recov_sbm.R
@@ -1,182 +1,139 @@
-library(nnet)
-#library(devtools)
-library(blockmodels)
-library(aricode)
+library("nnet")
+library("blockmodels")
+library("aricode")
 library("sbm")
 
-set.seed(1234)
-n=200
-p = 3
-K = 3
-X = matrix(1,n,p+1)
-for(j in 2:(p+1)){
-  X[,j] = rnorm(n,0,1/2)
+n <- 200
+p <- 3
+K <- 3
+
+alpha_cp <- matrix(c(
+  0.9, 0.5, 0.05,
+  0.3, 0.2, 0.1,
+  0.05, 0.01, 0
+), byrow = TRUE, nrow = K)
+
+alpha_as <- matrix(c(
+  0.7, 0.1, 0.05,
+  0.08, 0.5, 0.02,
+  0.05, 0.08, 0.25
+), byrow = TRUE, nrow = K)
+
+alpha_dis <- matrix(c(
+  0.08, 0.45, 0.9,
+  0.8, 0.05, 0.15,
+  0.5, 0.7, 0.01
+), byrow = TRUE, nrow = K)
+
+alphas_matrices <- list(assortative = alpha_as, core_periphery = alpha_cp, disassortative = alpha_dis)
+
+reorder_beta <- function(fit) {
+  max_idx <- which.max(fit$ICL)
+  fit$memberships[[max_idx]]$B
+
+  orderLabels <- order(colMeans(fit$memberships[[max_idx]]$alpha) %*%
+    fit$model_parameters[[max_idx]]$pi, decreasing = TRUE)
+  beta_hat <- matrix(fit$memberships[[max_idx]]$B[, orderLabels], ncol = max_idx)
+
+  beta_tilde <- beta_hat - beta_hat[, ncol(beta_hat)]
+  beta_tilde
 }
 
-Beta = round(matrix(abs(rnorm((p+1)*K,0,3)),(p+1),K))
-Beta[,K] = 0
-Sign_Beta  = matrix(0,p+1,K)
-for (i in 1:(p+1))
+seeds <- seq(1L, 100L)
+alphas <- seq_along(alphas_matrices)
+conditions <- expand.grid(seed = seeds, alpha = alphas)
+library(future.apply)
+library(future.callr)
+plan("callr")
+results_df <- future_lapply(seq_len(nrow(conditions)), function(c_idx) {
+  alpha_idx <- conditions[c_idx, "alpha"]
+  seed <- conditions[c_idx, "seed"]
+  alpha <- alphas_matrices[[alpha_idx]]
+  message(c_idx, "/", nrow(conditions), ": ", "seed ", seed, " and ", names(alphas_matrices)[alpha_idx], " structure.")
+  set.seed(seed)
+
+  X <- matrix(1, n, p + 1)
+  for (j in 2:(p + 1)) {
+    X[, j] <- rnorm(n, 0, 1 / 2)
+  }
+
+  Beta <- round(matrix(abs(rnorm((p + 1) * K, 0, 3)), (p + 1), K))
+  Beta[, K] <- 0
+  Sign_Beta <- matrix(0, p + 1, K)
+  for (i in 1:(p + 1))
   {
-  for (k in 1:K){
-    Sign_Beta[i,k]=(-1)^(k+i)}
-}
-Beta  = Beta*Sign_Beta
+    for (k in 1:K) {
+      Sign_Beta[i, k] <- (-1)^(k + i)
+    }
+  }
+  Beta <- Beta * Sign_Beta
+  nb_Beta_coeff <- nrow(Beta) * (ncol(Beta) - 1)
 
+  TAU <- exp(X %*% Beta)
+  Z <- sapply(1:n, function(i) {
+    sample(1:3, size = 1, replace = FALSE, TAU[i, ])
+  })
 
-#------------------------
-  
-TAU = exp(X%*% Beta)
-Z = sapply(1:n,function(i){sample(1:3,size=1,replace=FALSE,TAU[i,])})
+  Y <- matrix(rbinom(n * n, 1, alpha[Z, Z]), n, n)
 
+  # sbm
+  res_SBM <- estimateSimpleSBM(Y, model = "bernoulli", estimOptions = list(plot = FALSE, verbosity = 0))
+  alpha_sbm <- res_SBM$connectParam$mean
+  K_sbm <- ncol(alpha_sbm)
+  if (K_sbm != K) {
+    return(data.frame(seed = seed, alpha_struct = names(alphas_matrices)[alpha_idx], K_hat = K_sbm, ARI = c(NA, NA, NA), MSE = rep(NA, 3), model = c("sbm_multinom_taus", "sbm_multinom_Z", "blockmodels")))
+  }
+  ord_sbm <- order(apply(alpha_sbm, 1, sum), decreasing = TRUE)
+  tau_sbm <- res_SBM$probMemberships[, ord_sbm]
+  Z_sbm <- apply(res_SBM$indMemberships[, ord_sbm], 1, which.max)
 
-alpha <- matrix(c(0.9, 0.5, 0.05,
-              0.3, 0.2, 0.1,
-              0.05, 0.01, 0), byrow = TRUE, nrow = K)
-Y <- matrix(rbinom(n*n,1,alpha[Z,Z]),n,n)
+  ari_sbm <- ARI(Z, Z_sbm)
 
-# sbm
-res_SBM <- estimateSimpleSBM(Y,model="bernoulli", estimOptions = list(plot=FALSE))
-alpha_estim <- res_SBM$connectParam$mean
-ord_2 <- order(apply(alpha_estim,1,sum),decreasing = TRUE)
-tau_estim <- res_SBM$probMemberships[,ord_2]
-Z_estim <-apply(res_SBM$indMemberships[,ord_2],1,which.max)
-ARI(Z, Z_estim)
+  Beta_sbm_multinom_taus <- cbind(0, t(summary(multinom(tau_sbm ~ X[, 2] + X[, 3] + X[, 4], ))$coefficients))
+  Beta_sbm_multinom_taus <- Beta_sbm_multinom_taus - Beta_sbm_multinom_taus[, K_sbm]
+  Beta_sbm_multinom_Z <- cbind(rep(0, p + 1), t(summary(multinom(Z_sbm ~ X[, 2] + X[, 3] + X[, 4]))$coefficients))
+  Beta_sbm_multinom_Z <- Beta_sbm_multinom_Z - Beta_sbm_multinom_Z[, K]
 
-# blockmodels
-res_bm <- BM_bernoulli(membership_type = "SBM", adj = Y, 
-plotting='', nodes_covariates = list(node = X))
-res_bm$estimate()
-idx_max_ICL <- which.max(res_bm$ICL)
-ARI(Z, res_bm$memberships[[idx_max_ICL]]$map()$C)
-
-
-myTau_1 <- TAU*exp(matrix(rnorm(n*K,0,1),n,K))
-
-nu = 1000; 
-myTau_2 = t(sapply(1:n,function(i){
-  u =  rdirichlet(1,nu*TAU[i,]/sum(TAU[i,])); 
-  return(u)}
+  # blockmodels
+  res_bm <- BM_bernoulli(
+    membership_type = "SBM", adj = Y,
+    plotting = "", nodes_covariates = list(node = X), verbosity = 0
   )
-)
+  res_bm$estimate()
+  K_bm <- which.max(res_bm$ICL)
+  ari_bm <- ARI(Z, res_bm$memberships[[K_bm]]$map()$C)
+  Beta_bm <- reorder_beta(res_bm)
+  if (K_bm != K) {
+    MSE_Betas <- sapply(
+      list(Beta_sbm_multinom_taus, Beta_sbm_multinom_Z, matrix(NA, nrow(Beta), ncol(Beta))), function(Beta_test, Beta_ref = Beta, nb = nb_Beta_coeff) {
+        (1 / nb) * sum((Beta_ref - Beta_test)^2)
+      }
+    )
+  } else {
+    MSE_Betas <- sapply(
+      list(Beta_sbm_multinom_taus, Beta_sbm_multinom_Z, Beta_bm), function(Beta_test, Beta_ref = Beta, nb = nb_Beta_coeff) {
+        (1 / nb) * sum((Beta_ref - Beta_test)^2)
+      }
+    )
+  }
 
 
-myTau_1 <- myTau_1/myTau_1[,1]
-myTau_2 <- myTau_2/myTau_2[,1]
+  return(data.frame(seed = seed, alpha_struct = names(alphas_matrices)[alpha_idx], K_hat = c(K_sbm, K_sbm, K_bm), ARI = c(ari_sbm, ari_sbm, ari_bm), MSE = MSE_Betas, model = c("sbm_multinom_taus", "sbm_multinom_Z", "blockmodels")))
+}, future.seed = NULL) |> do.call(what = "rbind")
 
-head(TAU)
-head(myTau_1)
-head(myTau_2)
+library(dplyr)
+library(ggplot2)
+plot_data <- results_df %>%
+  na.omit() %>%
+  filter(ARI == 1)
+ggplot(
+  plot_data,
+  aes(x = as.factor(alpha_struct), y = MSE, fill = as.factor(model))
+) +
+  geom_violin(position = "dodge")
 
-myBeta_lin <- solve(t(X)%*%X)%*%t(X)%*%log(TAU)
-myBeta_1_lin <- solve(t(X)%*%X)%*%t(X)%*%log(myTau_1)
-myBeta_2_lin <- solve(t(X)%*%X)%*%t(X)%*%log(myTau_2)
-
-
-
-
-##### 
-
-res_estim <- multinom(TAU ~ X[,2] + X[,3]+X[,4])
-res_estim_myTau1 <- multinom(myTau_1 ~ X[,2] + X[,3] + X[,4])
-res_estim_myTau2 <- multinom(myTau_2 ~ X[,2] + X[,3] + X[,4])
-
-res_estim_Z <- multinom(Z ~ X[,2] + X[,3]+X[,4])
-
-
-#Beta_estim_multinom <- cbind(rep(0,p+1),t(summary(res_estim)$coefficients)) 
-#Beta_estim_multinom_Z <- cbind(rep(0,p+1),t(summary(res_estim_Z)$coefficients)) 
-# plot((X%*%Beta_estim_multinom), (X%*%Beta_estim_multinom_Z), main='Compar TAU / Z')
-
-
-
-Beta_estim_multinom_2<- cbind(rep(0,p+1),t(summary(res_estim_myTau2)$coefficients)) 
-Beta_estim_multinom_1<- cbind(rep(0,p+1),t(summary(res_estim_myTau1)$coefficients)) 
-
-
-#---------------------------------------- 
-par(mfrow=c(2,2))
-
-
-plot(log(TAU), (X%*%myBeta_lin), main='Exact softmax')
-points(log(TAU), (X%*%Beta_estim_multinom),col='green')
-abline(0,1)
-
-plot(log(myTau_1), (X%*%myBeta_1_lin), main='Presque softmax',col='red')
-points(log(myTau_1), (X%*%Beta_estim_multinom_1),col='green')
-abline(0,1)
-
-plot(log(myTau_2), (X%*%Beta_estim_multinom_2),col='green',main = "Dirichlet",xlim=range(X%*%Beta_estim_multinom_2))
-points(log(myTau_2),(X%*%myBeta_2_lin),col='red')
-abline(0,1)
-
-
-par(mfrow=c(2,2))
-
-plot(Beta, myBeta_lin, main='Exact softmax',type='p',col='red')
-points(Beta,Beta_estim_multinom, main='Exact softmax',type='p',col='green')
-abline(0,1)
-
-
-plot(Beta, myBeta_1_lin, main="Presque softmax",type='p',col='red')
-points(Beta,Beta_estim_multinom_1,type='p',col='green')
-abline(0,1)
-
-plot(Beta,myBeta_2_lin, main="Dirichlet",type='p',col='red')
-points(Beta,Beta_estim_multinom_2,type='p',col='green')
-abline(0,1)
-
-print(c(sum((Beta-myBeta_2_lin)^2),sum((Beta-Beta_estim_multinom_2)^2)))
-print(c(sum((Beta-myBeta_1_lin)^2),sum((Beta-Beta_estim_multinom_1)^2)))
-print(c(sum((Beta-myBeta_lin)^2),sum((Beta-Beta_estim_multinom)^2)))
-
-
-
-
-
-############" SBM 
-myTau_1 <- exp(X%*% Beta + matrix(rnorm(n*K,0,1.4),n,K))
-myTau_1 <- myTau_1/myTau_1[,1]
-Z = sapply(1:n,function(i){sample(1:3,size=1,replace=FALSE,TAU[i,])})
-
-alpha = round(matrix(rbeta(K*K,0.9,0.9),K,K),1)
-alpha = (alpha+t(alpha))/2
-ord <- order(apply(alpha,1,sum),decreasing = TRUE)
-alpha = alpha[ord,ord]
-
-Y <- matrix(rbinom(n*n,1,alpha[Z,Z]),n,n)
-
-res_SBM <- estimateSimpleSBM(Y,model="bernoulli")
-alpha_estim <- res_SBM$connectParam$mean
-ord_2 <- order(apply(alpha_estim,1,sum),decreasing = TRUE)
-tau_estim <- res_SBM$probMemberships[,ord_2]
-Z_estim <-apply(res_SBM$indMemberships[,ord_2],1,which.max)
-
-
-Beta_estim_SBM <- solve(t(X)%*%X)%*%t(X)%*%log(tau_estim/tau_estim[,1])
-Beta_estim_SBM_2 <- cbind(rep(0,p+1),t(summary(multinom(tau_estim ~ X[,2] + X[,3]+X[,4]))$coefficients))
-Beta_estim_SBM_3 <- cbind(rep(0,p+1),t(summary(multinom(Z_estim ~ X[,2] + X[,3]+X[,4]))$coefficients))
-
-
-
-
-
-
-par(mfrow=c(1,1))
-plot(Beta,Beta_estim_multinom)
-abline(a=0,b=1)
-points(Beta,Beta_estim_SBM,col='red')
-points(Beta,Beta_estim_SBM_2,col='blue',pch=2)
-points(Beta,Beta_estim_SBM_3,col='green',pch=3)
-
-#plot(log(tau_estim_2),log(TAU))
-
-R_SBM = sum((Beta-Beta_estim_SBM)^2)
-R_SBM_2 = sum((Beta-Beta_estim_SBM_2)^2)
-R_SBM_3 = sum((Beta-Beta_estim_SBM_3)^2)
-print(c(R_SBM,R_SBM_2,R_SBM_3))
-
-
-table(Z,Z_estim)
-
-
+ggplot(
+  plot_data %>% filter(alpha_struct != "disassortative"),
+  aes(x = as.factor(alpha_struct), y = MSE, fill = as.factor(model))
+) +
+  geom_violin(position = "dodge")
