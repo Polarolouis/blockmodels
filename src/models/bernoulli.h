dbde672ae2d8dc3a555f2dc4f5e916111ed86a62
Louis <louis.lacoste@agroparistech.fr>
2026-03-31 15:27:31 +0200

    Mask is checked as non zero when built and not per model

--- src/models/bernoulli.h
+++ src/models/bernoulli.h
@@ -36,9 +36,7 @@ class bernoulli
             }
 
             maskNA = compute_mask(adj_orig);
-            if (maskNA.is_zero()) {
-                Rcpp::stop("No valid edges in adjacency matrix. They are all NAs.");
-            }
+
             maskNAt = maskNA.t();
 
             adj = replace_missing_values(adj_orig, na_replace_value); // Replace NAs to perform
--- src/models/gaussian.h
+++ src/models/gaussian.h
@@ -69,9 +69,7 @@ class gaussian
             }
 
             maskNA = compute_mask(adj_orig);
-            if (maskNA.is_zero()) {
-                Rcpp::stop("No valid edges in adjacency matrix. They are all NAs.");
-            }
+
             maskNAt = maskNA.t();
 
             adj = replace_missing_values(adj_orig, na_replace_value); // Replace NAs to perform
